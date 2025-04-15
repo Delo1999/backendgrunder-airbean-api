b4ba7153956cac807cd861a12c45c49a0f85f09e
amber-85 <amber.an.bo@gmail.com>
2025-04-15 11:56:30 +0200

    updated profile page with styling

--- Frontend/src/components/profile/Profile.tsx
+++ Frontend/src/components/profile/Profile.tsx
@@ -34,37 +34,39 @@ const Profile: React.FC = () => {
   };
 
   return (
-    <div className="profileWrapper">
-      <h1>Välkommen till AirBean-familjen!</h1>
-      <h2>Logga in nedan för att se din orderhistorik.</h2>
-      <form className="form" onSubmit={handleSubmit}>
-        <label className="name">Användarnamn</label>
-        <input
-          type="text"
-          className="name"
-          placeholder="Användarnamn"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <label className="password">Lösenord</label>
-        <input
-          type="password"
-          className="password"
-          placeholder="Lösenord"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type="submit">Logga in</button>
-      </form>
-      {message && <p>{message}</p>}
-      <h2>
-        Inget konto? Skapa ett{" "}
-        <Link className="link" to="/register">
-          här
-        </Link>
-      </h2>
+    <div className="profile__wrapper">
+      <section className="profile__wrapper__frame">
+        <h1 className="profile__wrapper__frame__title">Välkommen till AirBean-familjen!</h1>
+        <h2 className="profile__wrapper__frame__subtitle">Logga in nedan för att se din orderhistorik.</h2>
+        <form className="profile__wrapper__frame__form" onSubmit={handleSubmit}>
+          <label className="profile__wrapper__frame__form__label">Användarnamn</label>
+          <input
+            type="text"
+            className="profile__wrapper__frame__form__input"
+            placeholder="Användarnamn"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <label className="profile__wrapper__frame__form__label">Lösenord</label>
+          <input
+            type="password"
+            className="profile__wrapper__frame__form__input"
+            placeholder="Lösenord"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit" className="profile__wrapper__frame__form__btn">Logga in</button>
+        </form>
+        {message && <p>{message}</p>}
+        <h2 className="profile__wrapper__frame__register">
+          Inget konto? Skapa ett{" "}
+          <Link className="link" to="/register">
+            här
+          </Link>
+        </h2>
+      </section>
     </div>
   );
 };
